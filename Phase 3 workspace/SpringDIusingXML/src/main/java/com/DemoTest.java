e0a47037cd18c6ddb536a2ceccb0870c10e66a6f
Akash <akash300383@gmail.com>
2022-08-16 12:54:53 +0530

    done

--- Phase 3 workspace/SpringDIusingXML/src/main/java/com/DemoTest.java
+++ Phase 3 workspace/SpringDIusingXML/src/main/java/com/DemoTest.java
@@ -1,5 +1,10 @@
 package com;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class DemoTest {
 
 	public static void main(String[] args) {
@@ -7,6 +12,25 @@ public class DemoTest {
 		//Employee emp1 = new Employee();
 		//emp1.display();
 
+		Resource rs = new ClassPathResource("beans.xml");  // loadin the xml file
+		BeanFactory bb = new XmlBeanFactory(rs);	// getting BeanFactory interface reference. 
+//		Employee employee1 = (Employee)bb.getBean("emp1");		// type casting 
+//		employee1.display();
+//		
+//		Employee employee3 = (Employee)bb.getBean("emp1");		// type casting 
+//		employee3.display();
+		
+//		Employee employee2 = (Employee)bb.getBean("emp2");
+//		employee2.display();
+//		
+//		Employee employee4 = (Employee)bb.getBean("emp2");		// type casting 
+//		employee4.display();
+		
+		Employee employee5 = (Employee)bb.getBean("emp1");
+		System.out.println(employee5);                    // it will call toString method 
+		
+		Employee employee6 = (Employee)bb.getBean("emp3");		// DI using parameterized constructor
+		System.out.println(employee6);						       
 		
 	}
 
--- Phase 3 workspace/SpringDIusingXML/src/main/java/com/Employee.java
+++ Phase 3 workspace/SpringDIusingXML/src/main/java/com/Employee.java
@@ -1,8 +1,30 @@
 package com;
 
 public class Employee {
+private int id;
+private String name;
+private float salary;
+	
+	public Employee() {
+		super();
+		System.out.println("Object created - using empty constructor");
+	}
+
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
 	public void display() {
 		System.out.println("Employee class method");
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
 }
