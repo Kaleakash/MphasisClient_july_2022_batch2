ec6e79c92e84d7d84c8c1f4f1b4c5bd244e97d82
Akash <akash300383@gmail.com>
2022-08-30 09:45:50 +0530

    done

--- Spring micro Service Project/EmployeeManagementSystemMicroServiceClient/EmployeeManagementSystemMicroServiceClient/src/main/java/com/controller/EmployeeController.java
+++ Spring micro Service Project/EmployeeManagementSystemMicroServiceClient/EmployeeManagementSystemMicroServiceClient/src/main/java/com/controller/EmployeeController.java
@@ -2,9 +2,16 @@ package com.controller;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +37,23 @@ public class EmployeeController {
 		return employeeService.getAllEmployee();
 	}
 	
+	@PostMapping(value = "storeEmployee",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String storeEmployee(@RequestBody Employee emp) {
+		return employeeService.storeEmployee(emp);
+	}
+	
+	@PutMapping(value = "updateEmployee",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateEmployee(@RequestBody Employee emp) {
+		return employeeService.updateEmployeeSalary(emp);
+	}
+	
+	@DeleteMapping(value = "deleteEmployee/{id}")
+	public String deleteEmployee(@PathVariable("id") int id) {
+		return employeeService.deleteEmployee(id);
+	}
+	
+	@GetMapping(value = "findEmployeeById/{id}")
+	public String findEmployee(@PathVariable("id") int id) {
+		return employeeService.findEmployee(id);
+	}
 }
--- Spring micro Service Project/EmployeeManagementSystemMicroServiceClient/EmployeeManagementSystemMicroServiceClient/src/main/java/com/service/EmployeeService.java
+++ Spring micro Service Project/EmployeeManagementSystemMicroServiceClient/EmployeeManagementSystemMicroServiceClient/src/main/java/com/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,58 @@ public class EmployeeService {
 	public List<Employee> getAllEmployee() {
 		return employeeRepository.findAll();
 	}
+	
+	public String storeEmployee(Employee emp) {
+		Optional<Employee> op = employeeRepository.findById(emp.getId());
+		if(op.isPresent()) {
+			return "Employee id must be unique";
+		}else {
+				employeeRepository.save(emp);				// insert new records
+				return "Record stored successfully";
+		}
+	}
+	
+	public String updateEmployeeSalary(Employee emp) {
+		Optional<Employee> op = employeeRepository.findById(emp.getId());
+		if(op.isPresent()) {
+				 	Employee e	= op.get();
+				 	e.setSalary(emp.getSalary());
+				 	employeeRepository.saveAndFlush(e);			// update 	existing record update property 
+				 	return "Record updated successfully";
+		}else {
+				
+				return "Record not present";
+		}
+	}
+	
+	public String deleteEmployee(int id) {
+		Optional<Employee> op = employeeRepository.findById(id);
+		if(op.isPresent()) {
+						Employee e = op.get();
+						employeeRepository.delete(e);
+			return "Employee deleted successfully";
+		}else {
+				return "Record not present";
+		}
+	}
+	
+	public String findEmployee(int id) {
+		Optional<Employee> op = employeeRepository.findById(id);
+		if(op.isPresent()) {
+			Employee e = op.get();
+			return e.toString();
+		}else {
+			return "Record not present";
+		}
+	}
+	
+	/*public Employee findEmployeeById(int id) {
+		Optional<Employee> op = employeeRepository.findById(id);
+		if(op.isPresent()) {
+			Employee e = op.get();
+			return e;
+		}else {
+			return null;
+		}
+	}*/
 }
